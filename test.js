978deebff3629dafb96431bfe5fca3ac121200e0
emily doswell <emilydoswell@yahoo.co.uk>
2022-03-03 16:49:40 +0100

    dom-function

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
-let arr = [1, 2, 2, 3, 2, 3, 4, 5, 4, 1, 4, 5];
+// let arr = [1, 2, 2, 3, 2, 3, 4, 5, 4, 1, 4, 5];
 
-arr.forEach
+// arr.forEach((number) => {
+//   const array = [];
+//   array.push(number);
+//   array.forEach((array_number) => {
+//     if (number !== array_number) {
+//       console.log(number);
+   
+//       array.push(number);
+//     };
+//   });
+//   // console.log(array);
+// });
 
 // output arr = [1, 2, 3, 4, 5]
+
+let arr = [1, 2, 2, 3, 2, 3, 4, 5, 4, 1, 4, 5];
+let output = [...new Set(arr)];
+console.log(output); //[1, 2, 3, 4, 5]
