eabc118cc89d82f3922a0040b474a0b1f8e469bd
emily doswell <emilydoswell@yahoo.co.uk>
2022-03-20 15:47:17 +0100

    information to reach API

--- test.js
+++ test.js
@@ -81,3 +81,27 @@ setTimeout(() => {
    console.log('This message will always run last...');
 }, 2500);
 console.log('Second message!');
+
+// Information to reach API
+
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+
+}
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  };
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
