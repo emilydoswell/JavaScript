e390e2661e7a6dbe481dac96ed793ec5b55caa76
emily doswell <emilydoswell@yahoo.co.uk>
2022-04-09 13:38:12 +0200

    JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
+
+
 var wordToDisplay = "Codecademy";
 
 var red = [0, 100, 63];
@@ -9,6 +15,6 @@ var letterColors = [red, orange, green, blue, purple];
 
 drawName(wordToDisplay, letterColors);
 
-bubbleShape = 'circle';
+bubbleShape = 'square';
 
 bounceBubbles();
