30d1a6fc9d9d1c4f91f3483595512ca3539d6eb3
emily doswell <emilydoswell@yahoo.co.uk>
2022-04-15 16:33:42 +0200

    react

--- main.js
+++ main.js
@@ -18,3 +18,11 @@ drawName(wordToDisplay, letterColors);
 bubbleShape = 'square';
 
 bounceBubbles();
+
+const h1 = React.createElement(
+  "h1",
+  null,
+  "Hello, world"
+);
+
+ReactDOM.render(h1, document.getElementById('app'));
